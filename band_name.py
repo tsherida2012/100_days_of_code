0ff5d88030243201fbb1aa3cf978b78e630c7951
Thomas Sheridan <tsherida2012@gmail.com>
2023-12-23 20:41:21 -0500

    cleaned up a bit

--- band_name.py
+++ band_name.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
 
-    input_location = input('Where were you born: ')
+    input_location = input('Where were you born:')
     input_name = input('What was your first Pet named: ')
     print(f'{input_location} {input_name} is your new band name')
